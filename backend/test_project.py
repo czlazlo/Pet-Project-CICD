687c72a3fa8aea5481c9c81ab3861056b54a850c
lazlo <czinege.laszlo3@gmail.com>
2023-07-10 17:30:40 +0200

    adds a simple pytest for the CI part of project

---
+++ backend/test_project.py
@@ -0,0 +1,8 @@
+from server import app
+
+
+def test_starting_page():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert b'Hello World' in response.data
+    print(response.data)
